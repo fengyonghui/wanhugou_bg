98c76bf3e599db8d1d6b9f3c5f4a7f48cdcc20a8
zhangtengfei <zhangtengfei_cn@163.com>
2018-04-11 11:14:55 +0800

    更新

--- src/main/java/com/wanhutong/backend/modules/sys/web/UserController.java
+++ src/main/java/com/wanhutong/backend/modules/sys/web/UserController.java
@@ -7,7 +7,6 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
-import javax.management.relation.RoleList;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.validation.ConstraintViolationException;
