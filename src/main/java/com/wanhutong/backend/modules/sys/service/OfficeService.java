490cf17599a5ee38fcd7cdfca81a720e15ea435e
zhangtengfei <zhangtengfei_cn@163.com>
2018-04-19 17:53:03 +0800

    会员列表权限

--- src/main/java/com/wanhutong/backend/modules/sys/service/OfficeService.java
+++ src/main/java/com/wanhutong/backend/modules/sys/service/OfficeService.java
@@ -142,7 +142,7 @@ public class OfficeService extends TreeService<OfficeDao, Office> {
 				}
 			}
 			BizCustomCenterConsultant customCenterConsultant=new BizCustomCenterConsultant();
-			if(flag && (source==null || source.equals(""))){
+			if(flag && (source==null || source.equals("") || source.equals("purchaser"))){
 				customCenterConsultant.setCenters(user.getCompany());
 
 				List<Office> officeList = officeDao.findOfficeByIdToParent(customCenterConsultant);
