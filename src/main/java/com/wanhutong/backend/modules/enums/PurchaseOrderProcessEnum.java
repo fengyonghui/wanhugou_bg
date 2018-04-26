78ccdaf45f2d241b5c0e6a9535f026dc0c741d76
Ma.Qiang <757000912@qq.com>
2018-04-26 16:36:22 +0800

    1.供应商上传产品优化

---
+++ src/main/java/com/wanhutong/backend/modules/enums/PurchaseOrderProcessEnum.java
@@ -0,0 +1,94 @@
+package com.wanhutong.backend.modules.enums;
+
+
+import com.google.common.collect.Maps;
+
+import java.util.Map;
+
+/**
+ * 采购单审核流程状态
+ * @author Ma.Qiang
+ */
+public enum  PurchaseOrderProcessEnum {
+
+    /**
+     * 采购中心负责人审批
+     */
+    PURCHASE_CENTER (1, RoleEnNameEnum.P_CENTER_MANAGER, 2),
+    /**
+     * 供货部
+     */
+    PROVIDER_CENTER (2, RoleEnNameEnum.PROVIDER_MANAGER, 3),
+    /**
+     * 会计
+     */
+    FINANCE (3, RoleEnNameEnum.FINANCE, 4),
+    /**
+     * 运营总监
+     */
+    OPERATION (4, RoleEnNameEnum.OP_DIRECTOR, 5),
+    /**
+     * 总经理
+     */
+    GENERAL_MANAGER (5, RoleEnNameEnum.GENERAL_MANAGER, 6),
+    /**
+     * 财会待付款
+     */
+    PAYMENT (6, RoleEnNameEnum.FINANCE, 7),
+    /**
+       * 支付完成
+       */
+    FINISH (7, null, 0),
+    ;
+
+    /**
+     * 数据MAP
+     */
+    public static final Map<Integer, PurchaseOrderProcessEnum> PROCESS_ENUM_MAP = getEnumMap();
+
+
+    /**
+     * 状态码
+     */
+    private int code;
+
+    /**
+     * 处理角色
+     */
+    private RoleEnNameEnum roleEnNameEnum;
+
+
+    /**
+     * 待处理状态
+     */
+    private int nextCode;
+
+
+    PurchaseOrderProcessEnum(int code, RoleEnNameEnum roleEnNameEnum, int nextCode) {
+        this.code = code;
+        this.roleEnNameEnum = roleEnNameEnum;
+        this.nextCode = nextCode;
+    }
+
+    public int getCode() {
+        return code;
+    }
+
+    public RoleEnNameEnum getRoleEnNameEnum() {
+        return roleEnNameEnum;
+    }
+
+    public int getNextCode() {
+        return nextCode;
+    }
+
+
+    private static Map<Integer, PurchaseOrderProcessEnum> getEnumMap() {
+        Map<Integer, PurchaseOrderProcessEnum> result = Maps.newHashMap();
+        for (PurchaseOrderProcessEnum e : values()) {
+            result.putIfAbsent(e.getCode(), e);
+        }
+        return result;
+    }
+}
+
--- src/main/java/com/wanhutong/backend/modules/enums/RoleEnNameEnum.java
+++ src/main/java/com/wanhutong/backend/modules/enums/RoleEnNameEnum.java
@@ -1,13 +1,53 @@
 package com.wanhutong.backend.modules.enums;
 
 public enum RoleEnNameEnum {
+    /**
+     * 采购中心经理
+     */
     P_CENTER_MANAGER("p_center_manager"),
+    /**
+     * 采购专员
+     */
     BUYER("buyer"),
+    /**
+     * 运营总监
+     */
+    OP_DIRECTOR("op_director"),
+    /**
+     * 总经理
+     */
+    GENERAL_MANAGER("general_manager"),
+    /**
+     * 财务
+     */
+    FINANCE("finance"),
+    /**
+     * 备货专员
+     */
     STOCKREADYCOMMISSIONER("stock_ready_commissioner"),
+    /**
+     * 采销经理
+     */
     MARKETINGMANAGER("marketing_manager"),
+    /**
+     * 仓储专员
+     */
     WAREHOUSESPECIALIST("warehouse_specialist"),
+    /**
+     * 供货中心经理
+     */
+    PROVIDER_MANAGER("provider_manager"),
+    /**
+     * 供应商
+     */
     SUPPLY_CHAIN("supply-chain"),
-    DEPT("dept");//系统管理员
+    /**
+     * 系统管理员
+     */
+    DEPT("dept");
+
+
+
     private String state;
 
     RoleEnNameEnum(String state) {
