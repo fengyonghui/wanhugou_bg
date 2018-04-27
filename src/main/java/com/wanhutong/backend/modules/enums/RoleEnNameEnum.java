dcb0d5c43495de5134415a8a5345f1939ae146cd
Ma.Qiang <757000912@qq.com>
2018-04-27 16:03:29 +0800

    1.供应商产品复制

--- src/main/java/com/wanhutong/backend/modules/enums/RoleEnNameEnum.java
+++ src/main/java/com/wanhutong/backend/modules/enums/RoleEnNameEnum.java
@@ -4,70 +4,63 @@ public enum RoleEnNameEnum {
     /**
      * 采购中心经理
      */
-    P_CENTER_MANAGER("p_center_manager"),
+    P_CENTER_MANAGER("p_center_manager", "采购中心经理"),
     /**
      * 采购专员
      */
-    BUYER("buyer"),
+    BUYER("buyer", "采购专员"),
     /**
      * 运营总监
      */
-    OP_DIRECTOR("op_director"),
+    OP_DIRECTOR("op_director", "运营总监"),
     /**
      * 总经理
      */
-    GENERAL_MANAGER("general_manager"),
+    GENERAL_MANAGER("general_manager", "总经理"),
     /**
      * 财务
      */
-    FINANCE("finance"),
+    FINANCE("finance", "财务"),
     /**
      * 备货专员
      */
-    STOCKREADYCOMMISSIONER("stock_ready_commissioner"),
+    STOCKREADYCOMMISSIONER("stock_ready_commissioner", "备货专员"),
     /**
      * 采销经理
      */
-    MARKETINGMANAGER("marketing_manager"),
+    MARKETINGMANAGER("marketing_manager", "采销经理"),
     /**
      * 仓储专员
      */
-    WAREHOUSESPECIALIST("warehouse_specialist"),
+    WAREHOUSESPECIALIST("warehouse_specialist", "仓储专员"),
     /**
      * 供货中心经理
      */
-    PROVIDER_MANAGER("provider_manager"),
+    PROVIDER_MANAGER("provider_manager", "供货中心经理"),
     /**
      * 供应商
      */
-    SUPPLY_CHAIN("supply-chain"),
+    SUPPLY_CHAIN("supply-chain", "供应商"),
     /**
      * 系统管理员
      */
-    DEPT("dept");
+    DEPT("dept", "系统管理员");
 
 
 
     private String state;
+    private String desc;
 
-    RoleEnNameEnum(String state) {
-
+    RoleEnNameEnum(String state, String desc) {
         this.state=state;
-    }
-
-    public static void main(String[] args) {
-
-
-        System.out.println( P_CENTER_MANAGER.getState() );
-
-
+        this.desc=desc;
     }
 
     public String getState() {
         return state;
     }
 
-    public void setState(String state) {
-        this.state = state;
+    public String getDesc() {
+        return desc;
     }
 }
