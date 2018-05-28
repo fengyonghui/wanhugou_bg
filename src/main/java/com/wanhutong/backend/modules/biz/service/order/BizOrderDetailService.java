5f16904e1a1a1c2eb7d61fe299270cdf384ea61b
Ouyang <13765128640@163.com>
2018-05-28 20:48:11 +0800

    订单详情行号与商品数量一致

--- src/main/java/com/wanhutong/backend/modules/biz/service/order/BizOrderDetailService.java
+++ src/main/java/com/wanhutong/backend/modules/biz/service/order/BizOrderDetailService.java
@@ -85,6 +85,16 @@ public class BizOrderDetailService extends CrudService<BizOrderDetailDao, BizOrd
                         BizSkuInfo sku = bizSkuInfoService.get(skuId);
                         BizOrderDetail detailnew = new BizOrderDetail();
                         detailnew.setOrderHeader(bizOrderDetail.getOrderHeader());//order_header.id
+                        if(bizOrderDetail.getOrderHeader()!=null && bizOrderDetail.getOrderHeader().getId()!=null){
+                            List<BizOrderDetail> list = bizOrderDetailDao.findList(detailnew);
+                            if(list.size()!=0){
+                                for (BizOrderDetail orderDetail : list) {
+                                    if(orderDetail.getLineNo()!=null){
+                                        a=orderDetail.getLineNo();
+                                    }
+                                }
+                            }
+                        }
                         detailnew.setLineNo(++a);//行号
                         detailnew.setpLineNo(null);//bom产品
                         detailnew.setSkuInfo(sku);//sku产品
