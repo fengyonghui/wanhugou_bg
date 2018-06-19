e98d0d3406436824c45e99d7bc2dc20ad5ddd390
liuying <liuy0209@163.com>
2018-06-19 19:14:45 +0800

    用户生成二维码

--- src/main/java/com/wanhutong/backend/modules/sys/web/UserCodeController.java
+++ src/main/java/com/wanhutong/backend/modules/sys/web/UserCodeController.java
@@ -1,6 +1,8 @@
 package com.wanhutong.backend.modules.sys.web;
 
+import com.alibaba.druid.support.json.JSONUtils;
 import com.wanhutong.backend.common.utils.CloseableHttpClientUtil;
+import com.wanhutong.backend.common.utils.DsConfig;
 import com.wanhutong.backend.common.web.BaseController;
 import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpGet;
@@ -12,6 +14,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.io.IOException;
+import java.util.Map;
 
 @Controller
 @RequestMapping(value = "${adminPath}/sys/userCode")
@@ -20,12 +23,12 @@ public class UserCodeController extends BaseController {
     @RequestMapping(value = "genUserQRCode")
     @ResponseBody
     public String genPayQRCode(Integer id){
-
         CloseableHttpClient httpClient= CloseableHttpClientUtil.createSSLClientDefault();
-        String getUrl="http://hh.ngrok.xiaomiqiu.cn/v1/wht/qrCode/"+id;
+        String getUrl= DsConfig.getUserGenCode()+id;
         HttpGet httpGet = new HttpGet(getUrl);
         CloseableHttpResponse httpResponse = null;
         String result=null;
+        String base64Data=null;
         try {
 
             httpGet.addHeader(HTTP.CONTENT_TYPE, "application/json;charset=utf-8");
@@ -38,7 +41,11 @@ public class UserCodeController extends BaseController {
 
             logger.info("返回结果result================="+result);
 
-
+            Map map= (Map) JSONUtils.parse(result);
+            if("1".equals((map.get("status")).toString())){
+                base64Data= map.get("data").toString();
+                logger.info(base64Data);
+            }
 
         } catch (Exception e) {
             e.printStackTrace();
@@ -51,7 +58,7 @@ public class UserCodeController extends BaseController {
                 }
             }
         }
-       return result;
+       return base64Data;
     }
 
 
