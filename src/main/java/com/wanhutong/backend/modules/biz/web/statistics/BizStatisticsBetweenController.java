51615336476b0f346cb8ba0b3a63c7b4cfbc5460
Ma.Qiang <757000912@qq.com>
2018-07-12 14:55:27 +0800

    1.sql更新

--- src/main/java/com/wanhutong/backend/modules/biz/web/statistics/BizStatisticsBetweenController.java
+++ src/main/java/com/wanhutong/backend/modules/biz/web/statistics/BizStatisticsBetweenController.java
@@ -432,7 +432,7 @@ public class BizStatisticsBetweenController extends BaseController {
             long time2 = cal.getTimeInMillis();
             betweenDays = (time2 - time1) / (1000 * 3600 * 24);
         } catch (ParseException e) {
-            e.printStackTrace();
+            logger.error("between get order data error", e);
         }
 
         Long finalBetweenDays = betweenDays;
