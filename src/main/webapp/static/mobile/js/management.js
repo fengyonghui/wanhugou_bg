6a704fb9645158c1aba6d471ce783c29bb72dded
Ma.Qiang <757000912@qq.com>
2018-07-23 19:09:39 +0800

    1.菜单

--- src/main/webapp/static/mobile/js/management.js
+++ src/main/webapp/static/mobile/js/management.js
@@ -25,10 +25,8 @@
                 data: {parentId:_this.userInfo.idData},
                 dataType: "json",
                 success: function(res){
-//                  console.log(res)
                     var htmlList = '';
                     $.each(res.data, function(i, item) {
-//                     		console.log(item)
                        		 htmlList += '<li class="mui-table-view-cell mui-collapse menuBtn" indexNum = "'+ i+'" dataId="'+item.id+'">'+
 									'<a class="mui-navigate-right">'+ item.name + '</a>'+
 									'<div  class = "mui-collapse-content childData'+ i+'">'
@@ -53,10 +51,8 @@
                         data: {parentId:dataId},
                         dataType: "json",
                         success: function(res){
-//                          console.log(res)
                             var pHtmlList = '';
                             $.each(res.data, function(i, item) {
-//                              console.log(item)
                                 pHtmlList+='<p class="childMenu" purchId="'+item.id+'">'+ item.name+'</p>'
 
                             });
