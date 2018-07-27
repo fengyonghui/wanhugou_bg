a7647ee7d2275f35e80ec50bbe65fab7a168ddd4
zhangtengfei <zhangtengfei_cn@163.com>
2018-07-27 15:46:52 +0800

    备货单拓展表

---
+++ src/main/java/com/wanhutong/backend/modules/biz/dao/request/BizRequestExpandDao.java
@@ -0,0 +1,29 @@
+/**
+ * Copyright &copy; 2017 <a href="www.wanhutong.com">wanhutong</a> All rights reserved.
+ */
+package com.wanhutong.backend.modules.biz.dao.request;
+
+import com.wanhutong.backend.common.persistence.CrudDao;
+import com.wanhutong.backend.common.persistence.annotation.MyBatisDao;
+import com.wanhutong.backend.modules.biz.entity.request.BizRequestExpand;
+import org.apache.ibatis.annotations.Param;
+
+import java.math.BigDecimal;
+
+/**
+ * 备货单扩展DAO接口
+ * @author TengFei.zhang
+ * @version 2018-07-26
+ */
+@MyBatisDao
+public interface BizRequestExpandDao extends CrudDao<BizRequestExpand> {
+
+    int incrPayTotal(@Param("id")int id, @Param("payTotal")BigDecimal payTotal);
+
+    int updatePaymentOrderId(@Param("id") Integer id, @Param("paymentId")Integer paymentId);
+
+    int getIdByRequestHeaderId(@Param("requestId") Integer requestId);
+
+
+	
+}
