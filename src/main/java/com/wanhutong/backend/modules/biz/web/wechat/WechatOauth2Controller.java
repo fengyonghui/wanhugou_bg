ba03e684ca0192b9706b3dc9353a9da4b65fe131
Ma.Qiang <757000912@qq.com>
2018-08-02 10:30:04 +0800

    1.跳转静态页面

--- src/main/java/com/wanhutong/backend/modules/biz/web/wechat/WechatOauth2Controller.java
+++ src/main/java/com/wanhutong/backend/modules/biz/web/wechat/WechatOauth2Controller.java
@@ -1,10 +1,12 @@
 package com.wanhutong.backend.modules.biz.web.wechat;
 
+import com.wanhutong.backend.common.utils.StringUtils;
 import com.wanhutong.backend.common.web.BaseController;
 import com.wanhutong.backend.modules.biz.entity.wechat.UserAccessToken;
 import com.wanhutong.backend.modules.biz.service.wechat.WechatOauth2Service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -39,14 +41,19 @@ public class WechatOauth2Controller extends BaseController {
      *
      * @return
      */
-    @RequestMapping(value = "/test", produces = "application/json; charset=utf-8")
-    public String registerPage(@RequestParam(value = "code", required = false) String code,
-                               @RequestParam(value = "state", required = false) String state) {
+    @RequestMapping(value = "/go2StaticPage_{path}", produces = "application/json; charset=utf-8")
+    public String registerPage(@PathVariable("path") String path,
+                               @RequestParam(value = "code", required = false) String code,
+                               @RequestParam(value = "state", required = false) String state
 
-        UserAccessToken accessToken = wechatOauth2Service.getUserInfoAccessToken(code);
-        System.out.println(accessToken);
-
-        return "redirect:/static/mobile/html/backstagemgmt.html";
+    ) {
+        String openId = null;
+        if (StringUtils.isNotBlank(code)) {
+            UserAccessToken accessToken = wechatOauth2Service.getUserInfoAccessToken(code);
+            openId = accessToken.getOpenid();
+        }
+        path = path.replaceAll("-", "/");
+        return "redirect:/static/mobile/html/" + path + ".html?state=" + state + "&openId=" + openId;
     }
 
 
