60c065b9b764cc58ca102b9de7c7f38709a769de
zhusiming <2476951877@qq.com>
2018-09-05 17:07:28 +0800

    状态流程修改

--- src/main/webapp/static/mobile/js/staffMgmtJs/orderJs/staOrdDetail.js
+++ src/main/webapp/static/mobile/js/staffMgmtJs/orderJs/staOrdDetail.js
@@ -138,7 +138,7 @@
 						    '</div>'+
 							'<div class="mui-input-row">'+
 						        '<label>状态:</label>'+
-						        '<input type="text" class="mui-input-clear" disabled value="'+ data.statusMap[item.bizStatus] +'">'+
+						        '<input type="text" class="mui-input-clear" disabled value="'+ data.stateDescMap[item.bizStatus] +'">'+
 						    	'<label>时间:</label>'+
 						        '<input type="text" value=" '+ _this.formatDateTime(item.createDate) +' " class="mui-input-clear" disabled>'+
 						    '</div>'+
