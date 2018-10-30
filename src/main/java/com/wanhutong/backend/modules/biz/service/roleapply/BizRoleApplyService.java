df195b8a0b09a1c4823d7ea019c29831bab786c9
王冰洋 <wangbingyang@wanhutong.com>
2018-10-30 12:57:45 +0800

    更新

--- src/main/java/com/wanhutong/backend/modules/biz/service/roleapply/BizRoleApplyService.java
+++ src/main/java/com/wanhutong/backend/modules/biz/service/roleapply/BizRoleApplyService.java
@@ -5,6 +5,7 @@ package com.wanhutong.backend.modules.biz.service.roleapply;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -21,6 +22,9 @@ import com.wanhutong.backend.modules.biz.dao.roleapply.BizRoleApplyDao;
 @Service
 @Transactional(readOnly = true)
 public class BizRoleApplyService extends CrudService<BizRoleApplyDao, BizRoleApply> {
+
+	@Autowired
+	private BizRoleApplyDao bizRoleApplyDao;
 
 	public BizRoleApply get(Integer id) {
 		return super.get(id);
@@ -42,6 +46,15 @@ public class BizRoleApplyService extends CrudService<BizRoleApplyDao, BizRoleApp
 	@Transactional(readOnly = false)
 	public void delete(BizRoleApply bizRoleApply) {
 		super.delete(bizRoleApply);
+	}
+
+	/**
+	 * 申请人的officeId 获取零售申请实体类
+	 * @param officeId
+	 * @return
+	 */
+	public List<BizRoleApply> getByOfficeId(Integer officeId) {
+		return bizRoleApplyDao.getByOfficeId(officeId);
 	}
 	
 }
